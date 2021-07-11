b3ce7612c63c22a76f6fc3ca3b38a3aa1871a4ee
steve <steve.braco@gmail.com>
2021-07-11 21:29:18 +0200

    publish on react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
+import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage'
+import CartPage from './pages/CartPage'
+import ProductsPage from './pages/ProductsPage'
+import SingleProductPage from './pages/SingleProductPage'
+import CheckoutPage from './pages/CheckoutPage'
+import ErrorPage from './pages/ErrorPage'
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return <div>
+    <Router>
+      <Navbar/>
+      <Sidebar/>
+      <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/about' component={AboutPage} />
+      <Route exact path='/cart' component={CartPage} />
+      <Route exact path='/products' component={ProductsPage} />
+      <Route exact path='/products/:id' component={SingleProductPage} />
+      <Route exact path='/checkout' component={CheckoutPage} />
+      <Route  path='*' component={ErrorPage}/>
+      </Switch>
+      <Footer/>
+    </Router>
+  </div>
 }
 
 export default App
