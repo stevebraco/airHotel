4baf1c3171e252d26b325283b909672a5cb11408
steve <steve.braco@gmail.com>
2021-07-14 15:31:11 +0200

    Filter Sort

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -17,6 +17,49 @@ const filter_reducer = (state, action) => {
       filtered_products: [...action.payload],
     };
   }
+
+  if(action.type === SET_GRIDVIEW) {
+    return {...state, grid_view: true}
+  }
+
+  if(action.type === SET_LISTVIEW) {
+    return {...state, grid_view: false}
+  }
+
+  if(action.type === UPDATE_SORT) {
+    return {...state, sort: action.payload}
+  }
+
+  if(action.type === SORT_PRODUCTS) {
+    const {sort, filtered_products} = state;
+    let tempProducts = [...filtered_products];
+    if(sort === 'price-lowest') {
+      console.log('price-lowest');
+      // I want to store them based on price by placing the smallest item first
+      // We are comparing Two values, the current value and the next value
+      tempProducts = tempProducts.sort((a,b) => a.price - b.price )
+    }
+    if(sort === 'price-highest') {
+      // I want to store them based on price by placing the highest item first
+      tempProducts = tempProducts.sort((a,b) => b.price - a.price )
+
+      console.log('price-highest');
+    }
+    if(sort === 'name-a') {
+      tempProducts = tempProducts.sort((a,b) => {
+        return a.name.localeCompare(b.name)
+      })
+      console.log('name-a');
+    }
+    if(sort === 'name-z') {
+      tempProducts = tempProducts.sort((a,b) => {
+        return b.name.localeCompare(a.name)
+      })
+      console.log('name-z');
+    }
+    return {...state, filtered_products: tempProducts}
+  }
+
   throw new Error(`No Matching "${action.type}" - action type`);
 };
 
